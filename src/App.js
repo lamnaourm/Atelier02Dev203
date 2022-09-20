2cb2eeff77c2c4fe2352f8f29feaaaa9fb19a1b1
lamnaourm <mohammed.lamnaour@gmail.com>
2022-09-20 11:47:20 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+var etudiants = [
+  { nom: "Manar", math: 10.5, phys: 12.0, fr: 16.0 },
+  { nom: "Mohamed", math: 13.0, phys: 18.0, fr: 7.0 },
+  { nom: "Khalid", math: 15.0, phys: 15.0, fr: 13.0 },
+  { nom: "Ikbal", math: 2.6, phys: 9.0, fr: 10.0 },
+  { nom: "Aziz", math: 10.0, phys: 11.5, fr: 15.0 },
+  { nom: "Said", math: 12.0, phys: 5.0, fr: 13.0 },
+  { nom: "Moujahid", math: 20.0, phys: 15.0, fr: 18.0 },
+  { nom: "Riyad", math: 18.0, phys: 13.0, fr: 15.0 },
+];
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
